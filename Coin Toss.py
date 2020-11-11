ff761ed483cb0029d00a83bb0df365b07e0d054e
Leboweeb <74187782+Leboweeb@users.noreply.github.com>
2020-11-11 16:18:46 +0200

    Add files via upload

--- Coin Toss.py
+++ Coin Toss.py
@@ -1,29 +1,38 @@
 import random as r
 
-list1 = ["heads", "tails"]
+mode = input("Do you want to make the game endless?:")
 
-tot = 0
-try:
 
-    n = input("What is your guess?:\n ")
-    if type(n) != str:
-        raise TypeError("Please write a word")
-    if n not in ["heads", "tails", "Tails", "Heads"]:
-        raise TypeError("Please write the guess properly")
-    a = int(input("How rich are you?\n(How many coins can you flip?)\n "))
-    # rn is the number of rounds
-    for i in range(a):
-        b = r.choice(list1)
-        if n == b:
-            tot += 1
-        elif n.lower() == b:
-            tot += 1
-        else:
-            tot = tot
-except ValueError:
-    print("Please enter a valid number")
-except TypeError:
-    print("Please Enter A Word")
+def coin():
+    tot = 0
+    list1 = ["heads", "tails"]
+    try:
 
-print(f"The game has ended. You got a score of {tot}.")
+        n = input("What is your guess?:\n ")
+        if n == "quit":
+            quit()
+        while n not in ["heads", "tails", "Tails", "Heads"]:
+            raise KeyError
+
+        a = int(input("How rich are you?\n(How many coins can you flip?)\n "))
+        for i in range(a):
+            b = r.choice(list1)
+            if n == b:
+                tot += 1
+            elif n.lower() == b:
+                tot += 1
+            else:
+                tot = tot
+    except ValueError:
+        print("Please enter a valid number")
+    except KeyError:
+        print("Please write the guess properly in uppercase or lowercase")
+
+    print(f"The game has ended. You got a score of {tot}.")
+
+
+while mode in ["Yes", "yes"]:
+    coin()
+if mode in ["No", "no"]:
+    coin()
 
